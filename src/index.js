c3384acd612f4983e9665a664e6bddfea91c1a4f
Brian Cavalier <brian@briancavalier.com>
2017-09-16 22:08:06 -0400

    chore(unfold): remove unnecessary mutual recursion

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ export const unfold = <A, B>(f: B => [Stream<A>, B], b: B): Stream<A> =>
   stepUnfold(f, f(b))
 
 const stepUnfold = <A, B>(f: B => [Stream<A>, B], [s, b]: [Stream<A>, B]): Stream<A> =>
-  continueWith(() => unfold(f, b), s)
+  continueWith(() => stepUnfold(f, f(b)), s)
